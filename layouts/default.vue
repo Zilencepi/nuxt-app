ac043310e92fa6c675e059ce3e08d734cc074322
Zivojin <zilencepi@gmail.com>
2023-11-11 20:28:50 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+   <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+   </nav>
+   <main>
+     <slot />
+   </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
